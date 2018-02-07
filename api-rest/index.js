e1863cbbdc8e9161e53f3a99807d51f568385cb6
karlosmaor <karlosmaor94@gmail.com>
2018-02-07 13:26:34 -0500

    50

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,17 +1,16 @@
 'use strict'
 
 const mongoose =  require('mongoose')
-
 const app  = require('./app')
+const config = require('./config')
 
-const port = process.env.PORT || 3001
-
-mongoose.connect('mongodb://localhost:27017/carlosShop', (err,res)=>{
+mongoose.connect(config.db, (err,res)=>{
   if(err){
     return console.log(`Error al conectarse a la base de datos: ${err}`);
   }
   console.log('Conexion a la base de datos exitosa...')
-  app.listen(port, () => {
+
+  app.listen(config.port, () => {
     console.log('API REST corriendo exitosamente')
   })
 })
