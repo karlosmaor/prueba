2eec893b540cf91cbb6f33727014149dcb2bf550
karlosmaor <karlosmaor94@gmail.com>
2018-02-07 11:00:05 -0500

    23

--- api-rest/index.js
+++ api-rest/index.js
@@ -15,9 +15,10 @@ app.get('/api/product', (req,res) =>{
   Product.find({}, (err, products)=>{
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
     if(!products)return res.status(404).send({message:'No existen productos'})
-    
+
+    res.status(200).send({products})
   })
-  res.status(200).send({products})
+
 
 })
 
