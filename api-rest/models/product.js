2f03d27837c58076ea0d9957604ee7f59de23bb5
karlosmaor <karlosmaor94@gmail.com>
2018-02-07 10:31:53 -0500

    product export

--- api-rest/models/product.js
+++ api-rest/models/product.js
@@ -10,3 +10,5 @@ const ProductSchema = Schema ({
   category: { type: String, enum: ['computers' , 'phones' , 'accesories'] },
   description: String
 })
+
+module.exports = mongoose.model('Product',ProductSchema)
