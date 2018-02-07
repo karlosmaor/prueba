2664c137dd6df7559f1e70b76f54407d1a18e673
karlosmaor <karlosmaor94@gmail.com>
2018-02-07 12:28:59 -0500

    42

--- api-rest/routes/index.js
+++ api-rest/routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const api = express.Router()
 
-const ProductCtrl = require('./controles/product')
+const ProductCtrl = require('../controles/product')
 
 api.get('/product', ProductCtrl.getProducts)
 
