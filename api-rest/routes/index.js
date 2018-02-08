62d16fbcdda25fcdf60c74065a18c5868fa3b8a4
karlosmaor <karlosmaor94@gmail.com>
2018-02-08 10:46:13 -0500

    prueba SignUp

--- api-rest/routes/index.js
+++ api-rest/routes/index.js
@@ -5,17 +5,15 @@ const api = express.Router()
 const auth = require('../middleswares/auth')
 
 const ProductCtrl = require('../controles/product')
+const UserCtrl  = require('../controles/user')
 
 api.get('/product', ProductCtrl.getProducts)
-
 api.get('/product/:productId', ProductCtrl.getProduct)
-
 api.post('/product', ProductCtrl.saveProduct)
-
 api.put('/product/:productId', ProductCtrl.updatePoduct)
-
 api.delete('/product/:productId', ProductCtrl.deleteProduct)
-
+api.post('/signup', UserCtrl.sigUp)
+api.post('/signin', UserCtrl.signIn)
 api.get('/private', auth, function(req,res){
   res.status(200).send({message:'Tienes acceso'})
 })
